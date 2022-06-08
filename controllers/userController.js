f4d5fd12baacb1f25108695f3b546c01e770dcc6
NazmusSakib-tech <sakibkhancse09@gmail.com>
2022-06-08 16:48:33 +0600

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,6 +16,49 @@ const addUser = async (req, res) => {
   res.status(200).json(newUser);
 };
 
+//2. update user
+const updateUser = async (req, res) => {
+  let userInfo = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  const updatedUser = await User.update(userInfo, {
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json(updatedUser);
+};
+
+//3. find all users
+const findAllUsers = async (req, res) => {
+  const allUsers = await User.findAll();
+  res.status(200).json(allUsers);
+};
+
+//4 Delete user
+const deleteUser = async (req, res) => {
+  const deletedUser = await User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json(deletedUser);
+};
+
+//4. Delete all users
+const deleteAllUsers = async (req, res) => {
+  const deletedAllUsers = await User.destroy({
+    where: {},
+  });
+  res.status(200).json(deletedAllUsers);
+};
+
 module.exports = {
   addUser,
+  updateUser,
+  findAllUsers,
+  deleteUser,
+  deleteAllUsers,
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,5 +3,9 @@ const userController = require("../controllers/userController");
 const router = require("express").Router();
 
 router.post("/add-user", userController.addUser);
+router.put("/update-user/:id/", userController.updateUser);
+router.get("/all-users", userController.findAllUsers);
+router.delete("/delete-user/:id/", userController.deleteUser);
+router.delete("/delete-all-users/", userController.deleteAllUsers);
 
 module.exports = router;
